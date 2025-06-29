2a291201b133ac482df4154214924f2a3b73213a
nurlailihub <nurlailichaniago456@gmail.com>
2025-06-29 19:10:38 +0700

    templates by lili

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Footer AdminLTE -->
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Versi 1.0
+    </div>
+    <strong>&copy; 2024 <a href="/">AdminLTE Laravel</a>.</strong> All rights reserved.
+</footer> 
